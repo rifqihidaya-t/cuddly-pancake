f3faf0fe4cb408a4a66bf9bfa9e0538af9806c72
rifqihidaya-t <191080200147@umsida.ac.id>
2025-08-08 21:15:48 +0700

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt upgrade 
+add-apt-repository multiverse
+sudo apt update
+sudo apt install build-essential git curl jq tmux htop fzf ripgrep bat eza ffmpeg ubuntu-restricted-extras openssh-server openssh-client
